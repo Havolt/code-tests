25dc0a9c50250091acb4f61f4adbe090e5d7ca5c
Mark <markfitz815@gmail.com>
2024-05-29 20:09:46 +0100

    Initial version of remove duplicates ii

--- remove-duplicates-sorted-array-ii/code.js
+++ remove-duplicates-sorted-array-ii/code.js
@@ -4,12 +4,18 @@
  */
 var removeDuplicates = function(nums) {
 
-   if(nums.length < 2) {
-      return;
+   if(nums.length <= 2) {
+      console.log(nums);
+      return nums;
    }
 
    for(let i = 2; i < nums.length; i++) {
-      console.log(nums[i], nums[i-1], nums[i-2]);
+      // console.log(nums[i], nums[i-1], nums[i-2]);
+      if(nums[i] === nums[i-1] && nums[i] === nums[i-2]) {
+         nums.splice(i, 1);
+         i--;
+      }
+      
    }
     console.log(nums);
 };
